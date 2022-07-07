c1a2666b5cf46e88f0c972124e44e2b24fbb7946
Justin Fowler <justinfowlerart@gmail.com>
2022-07-07 19:27:58 -0400

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 id='title' className='text-3xl font-bold underline'>
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
